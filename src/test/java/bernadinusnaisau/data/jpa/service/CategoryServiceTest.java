26f0fe4168d2d024e8726bfb426ff656321c7536
bernad <bernadinusnaisau230803@gmail.com>
2025-09-12 21:56:00 +0700

    Belajar-spring-data-jpa: Testing (Declarative Transaction)

---
+++ src/test/java/bernadinusnaisau/data/jpa/service/CategoryServiceTest.java
@@ -0,0 +1,29 @@
+package bernadinusnaisau.data.jpa.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+public class CategoryServiceTest {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Test
+    void validAddData() {
+     Assertions.assertThrows(RuntimeException.class, () -> {
+         categoryService.create();
+     });
+    }
+
+    @Test
+    void invalidAddData() {
+        assertThrows(RuntimeException.class, () -> {
+            categoryService.test();
+        });
+    }
+}
