7905c31ddab380e3667466c3c821744188c0021d
bernad <bernadinusnaisau230803@gmail.com>
2025-09-12 20:40:03 +0700

    Belajar-spring-data-jpa: Implementasi repository (membuat entity category)

--- src/main/java/bernadinusnaisau/data/jpa/entity/Category.java
+++ src/main/java/bernadinusnaisau/data/jpa/entity/Category.java
@@ -1,9 +1,6 @@
 package bernadinusnaisau.data.jpa.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,12 +10,13 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 @Table(name = "categories")
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     private String name;
 }
