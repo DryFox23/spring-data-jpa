1936b2fda86fae6967b55ce17f1101184942b32e
bernad <bernadinusnaisau230803@gmail.com>
2025-09-14 22:24:04 +0700

    Belajar-spring-data-jpa: Testing select data menggunakan unit test(Query Method)

---
+++ src/test/java/bernadinusnaisau/data/jpa/repository/CategoryRepositoryTest.java
@@ -0,0 +1,55 @@
+package bernadinusnaisau.data.jpa.repository;
+
+import bernadinusnaisau.data.jpa.entity.Category;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class CategoryRepositoryTest {
+    
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    void addData() {
+        Category category = new Category();
+        category.setName("GADGET");
+        categoryRepository.save(category);
+
+        assertNotNull(category.getId());
+    }
+
+    @Test
+    void updateData() {
+        Category category = categoryRepository.findById(1L).orElse(null);
+        assertNotNull(category);
+
+        category.setName("GADGET PRICEY");
+        categoryRepository.save(category);
+
+        category = categoryRepository.findById(1L).orElse(null);
+        assertEquals("GADGET PRICEY", category.getName());
+    }
+
+    @Test
+    void SelectQuery() {
+        Category category = categoryRepository.findFirstByNameEquals("GADGET PRICEY").orElse(null);
+        assertNotNull(category);
+        assertEquals("GADGET PRICEY", category.getName());
+
+        List<Category> categories = categoryRepository.findAllByNameLike("%GADGET%");
+        assertFalse(categories.isEmpty());
+        assertEquals("GADGET PRICEY", categories.get(0).getName());
+
+        Category categoryById = categoryRepository.findById(1L).orElse(null);
+        assertNotNull(categoryById);
+        assertEquals("GADGET PRICEY", categoryById.getName());
+
+    }
+}
