5ac23be67b4304717c34fb324b801127a6b355d9
bernad <bernadinusnaisau230803@gmail.com>
2025-09-15 22:04:10 +0700

    Belajar-spring-data-jpa: Testing select data menggnakan pageable serta hasil pagingnya(Paging Result)

--- src/test/java/bernadinusnaisau/data/jpa/repository/ProductRepositoryTest.java
+++ src/test/java/bernadinusnaisau/data/jpa/repository/ProductRepositoryTest.java
@@ -5,6 +5,7 @@ import bernadinusnaisau.data.jpa.entity.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -64,18 +65,39 @@ public class ProductRepositoryTest {
         assertEquals(1L, products.get(1).getId());
     }
 
-    @Test
-    void selectDataUsingPageable() {
-        //page 1
-        Pageable pageable = PageRequest.of(0, 1, Sort.by(Sort.Order.asc("id")));
-        List<Product> products = productRepository.findAllByCategory_Name("GADGET PRICEY", pageable);
-        assertEquals(1, products.size());
-        assertEquals("Xiaomi Redmi Note 12", products.get(0).getName());
+//    @Test
+//    void selectDataUsingPageable() {
+//        //page 1
+//        Pageable pageable = PageRequest.of(0, 1, Sort.by(Sort.Order.asc("id")));
+//        List<Product> products = productRepository.findAllByCategory_Name("GADGET PRICEY", pageable);
+//        assertEquals(1, products.size());
+//        assertEquals("Xiaomi Redmi Note 12", products.get(0).getName());
+//
+//        //page 2
+//        pageable = PageRequest.of(1, 1, Sort.by(Sort.Order.asc("id")));
+//        products = productRepository.findAllByCategory_Name("GADGET PRICEY", pageable);
+//        assertEquals(1, products.size());
+//        assertEquals("Samsung Galaxy A14", products.get(0).getName());
+//    }
 
-        //page 2
+    @Test
+    void selectDataUsingPageableWithCountPage() {
+//        Page 1
+        Pageable pageable = PageRequest.of(0, 1, Sort.by(Sort.Order.asc("id")));
+        Page<Product> products = productRepository.findAllByCategory_Name("GADGET PRICEY", pageable);
+        assertEquals(1, products.getContent().size());
+        assertEquals(0, products.getNumber());
+        assertEquals(2, products.getTotalElements());
+        assertEquals(2, products.getTotalPages());
+        assertEquals("Xiaomi Redmi Note 12", products.getContent().get(0).getName());
+
+//        Page 2
         pageable = PageRequest.of(1, 1, Sort.by(Sort.Order.asc("id")));
         products = productRepository.findAllByCategory_Name("GADGET PRICEY", pageable);
-        assertEquals(1, products.size());
-        assertEquals("Samsung Galaxy A14", products.get(0).getName());
+        assertEquals(1, products.getContent().size());
+        assertEquals(1, products.getNumber());
+        assertEquals(2, products.getTotalElements());
+        assertEquals(2, products.getTotalPages());
+        assertEquals("Samsung Galaxy A14", products.getContent().get(0).getName());
     }
 }
