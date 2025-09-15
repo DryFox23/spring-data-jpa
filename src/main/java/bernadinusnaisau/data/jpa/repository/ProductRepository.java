e8e849cc4b480eb7ababc2676e509401121c80b9
bernad <bernadinusnaisau230803@gmail.com>
2025-09-15 21:32:44 +0700

    Belajar-spring-data-jpa: Implementasi select data menggnakan pageable(Paging)

--- src/main/java/bernadinusnaisau/data/jpa/repository/ProductRepository.java
+++ src/main/java/bernadinusnaisau/data/jpa/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package bernadinusnaisau.data.jpa.repository;
 
 import bernadinusnaisau.data.jpa.entity.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,6 +12,6 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findAllByCategory_Name(String name);
-
+    List<Product> findAllByCategory_Name(String name, Pageable pageable);
     List<Product> findByCategory_Id(Long id, Sort sort);
 }
