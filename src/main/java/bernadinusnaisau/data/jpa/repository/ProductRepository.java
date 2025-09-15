48ea16b91a508cda20d1d9a6ea764a4013dfd077
bernad <bernadinusnaisau230803@gmail.com>
2025-09-15 20:29:47 +0700

    Belajar-spring-data-jpa: Implementasi query SQL di dalam repository dengan menulis nama method dengan relational (Query Relational)

---
+++ src/main/java/bernadinusnaisau/data/jpa/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package bernadinusnaisau.data.jpa.repository;
+
+import bernadinusnaisau.data.jpa.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findAllByCategory_Name(String name);
+
+    List<Product> findByCategory_Id(Long id);
+}
