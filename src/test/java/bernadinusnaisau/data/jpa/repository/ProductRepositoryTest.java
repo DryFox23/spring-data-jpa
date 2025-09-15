c112f5cf82af490f40968ad4b3b3c9c958de5550
bernad <bernadinusnaisau230803@gmail.com>
2025-09-15 20:32:07 +0700

    Belajar-spring-data-jpa: Testing insert data dan select data product(Query Relational)

---
+++ src/test/java/bernadinusnaisau/data/jpa/repository/ProductRepositoryTest.java
@@ -0,0 +1,62 @@
+package bernadinusnaisau.data.jpa.repository;
+
+import bernadinusnaisau.data.jpa.entity.Category;
+import bernadinusnaisau.data.jpa.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    void insertData() {
+        Category category = categoryRepository.findById(1L).orElse(null);
+        assertNotNull(category);
+
+        {
+            Product product = new Product();
+            product.setName("Xiaomi Redmi Note 12");
+            product.setPrice(3000000L);
+            product.setCategory(category);
+            productRepository.save(product);
+        }
+        {
+            Product product = new Product();
+            product.setName("Samsung Galaxy A14");
+            product.setPrice(2500000L);
+            product.setCategory(category);
+            productRepository.save(product);
+        }
+    }
+
+    @Test
+    void selectDataByProductName() {
+        List<Product> products = productRepository.findAllByCategory_Name("GADGET PRICEY");
+        assertNotNull(products);
+
+        assertEquals(2, products.size());
+        assertEquals("Xiaomi Redmi Note 12", products.get(0).getName());
+        assertEquals("Samsung Galaxy A14", products.get(1).getName());
+    }
+
+    @Test
+    void selectDataByProductId() {
+        List<Product> products = productRepository.findByCategory_Id(1L);
+        assertNotNull(products);
+
+        assertEquals(2, products.size());
+        assertEquals(1L, products.get(0).getId());
+        assertEquals(2L, products.get(1).getId());
+    }
+}
